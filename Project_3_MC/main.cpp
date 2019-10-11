893105477b7799e4c61ac5aebacfe2703a659c22
Øyvind <stoyvind@gmail.com>
2019-10-11 17:30:31 +0200

    added mc_2 for 3d

--- Project_3_MC/main.cpp
+++ Project_3_MC/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <armadillo>
 #include "lib.h"
-//#include "lib.cpp"
 #include <random>
 #include <chrono>
 
@@ -9,33 +8,34 @@ using namespace std;
 using namespace arma;
 using namespace chrono;
 
-double func(double x1,double y1,double z1, double x2, double y2, double z2 );
 
-
+// delcaration of function
+double func_brute(double x1,double y1,double z1, double x2, double y2, double z2 );
 
 
 int main()
 {
-    // computes monte carlo with a uniform distribution without important sampling
+    // computes Monte Carlo with a uniform distribution without important sampling
+
     int n;
     cout << "Input for number of Monte Carlo samples:" << endl;
     cin >> n;
     double MCint = 0;
-    double MCintsqr2 = 0;
+    double sum_sigma = 0;
     double fx = 0;
     const double pi =3.141592653589793238463;
     double exact_solution = 5*pi*pi / (16*16);
 
+
+
     // random number generator
-
-    typedef high_resolution_clock myclock;
-    myclock::time_point beginning = myclock::now();
-
     unsigned seed = system_clock::now().time_since_epoch().count();
     mt19937_64 generator (seed);
 
-/*
+
+
     // random number test
+/*
     for (int i = 0; i < n; i++){
         double g;
         g = generate_canonical< double, 128 > (generator);
@@ -47,8 +47,13 @@ int main()
 
 
 
+    // starting clock for time keeping
+    high_resolution_clock::time_point t1 = high_resolution_clock::now();
+
+
+
     for (int i = 1; i <= n; i++){
-        // generating random numbers between -2,2
+        // generating random numbers between -2,2 for each coordinate
         double g;
         g = generate_canonical< double, 128 > (generator);
         double x1 = double(g - 0.5)*4;
@@ -63,32 +68,39 @@ int main()
         g = generate_canonical< double, 128 > (generator);
         double z2 = double(g - 0.5)*4;
 
-        fx = func(x1,y1,z1,x2,y2,z2);
+
+
+        // MC integrating
+        fx = func_brute(x1,y1,z1,x2,y2,z2);
         MCint += fx;
-        MCintsqr2 += fx*fx;
+        sum_sigma += fx*fx;
     }
+
+    // stops the clock
+    high_resolution_clock::time_point t2 = high_resolution_clock::now();
+    duration<double> time_span = duration_cast<duration<double> >(t2 - t1);
+    cout << endl << "Monte Carlo brute force used " << time_span.count() << " seconds" << endl;
+    cout << endl;
+
+
+    // setting up the jaocbi
+    int a = -2;
+    int b = 2;
+    double jacobi = pow((b-a),6);
+
+    // calculating the mean integration results and the variance
     MCint = MCint / (double (n));
-    MCintsqr2 = MCintsqr2 / (double (n));
-    double variance = MCintsqr2 - MCint*MCint; // standard deviation
+    sum_sigma = sum_sigma / (double (n));
+    double variance = sum_sigma - MCint*MCint;
 
 
-
-    cout << "Variance = " << setw(25) << setprecision(20) << variance << endl
-         << "Integral = " << setw(25) << setprecision(20) << MCint << endl
+    cout << "Variance = " << variance << endl
+         << "Integral = " << jacobi * MCint << endl
          << "Exact = " << exact_solution << endl;
 
+
 }
 
-
-//
-double func(double x1, double y1,double z1, double x2, double y2, double z2){
-    double alpha = 2.0;
-    double r1 = sqrt(x1*x1 + y1*y1 + z1*z1);
-    double r2 = sqrt(x2*x2 + y2*y2 + z2*z2);
-    double r1_r2 = sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2));
-    if (r1_r2 < pow(10,-10)){return 0;}
-    double I = exp(-2*alpha*(r1+r2)) / r1_r2;
-    return I;
-}
+// end of main
 
 
---
+++ Project_3_MC/mc_2.cpp
@@ -0,0 +1,112 @@
+
+#include <iostream>
+#include <armadillo>
+#include "lib.h"
+#include <random>
+#include <chrono>
+
+using namespace std;
+using namespace arma;
+using namespace chrono;
+
+
+// delcaration of function
+double func_important_samp(double x1, double y1, double z1, double x2, double y2, double z2, double theta1, double theta2);
+double func_important_samp(double r1, double r2, double theta1, double theta2, double phi1, double phi2);
+
+
+
+// Monte Carlo with exponential distribution important sampling
+
+
+int main()
+{
+    // computes Monte Carlo with an exponential distribution without important sampling
+
+    int n;
+    cout << "Input for number of Monte Carlo samples:" << endl;
+    cin >> n;
+    double MCintIS = 0;
+    double sum_sigmaIS = 0;
+    double fy = 0;
+    const double pi =3.141592653589793238463;
+    double exact_solution = 5*pi*pi / (16*16);
+
+
+
+    // random number generator
+    unsigned seed = system_clock::now().time_since_epoch().count();
+    mt19937_64 generator (seed);
+
+    // starting clock for time keeping
+    high_resolution_clock::time_point t1 = high_resolution_clock::now();
+
+    for (int i = 1; i <= n; i++){
+        // generating r1 and r2 with the exponential distribution
+        double g;
+        g = generate_canonical< double, 128 > (generator);  // random number [0,1]
+        double r1 = -0.25*log(1.- g);                        // random number with exp dist
+        g = generate_canonical< double, 128 > (generator);
+        double r2 = -0.25*log(1.-g);
+
+        /*
+        cout << "r1 and r2 " << endl
+             << r1 << " " << r2 << endl;
+        */
+
+        // defining theta and phi
+        // Initialize the seed and call the Mersienne algo
+        random_device rd;
+        mt19937_64 gen(rd());
+
+        // Set up the uniform distribution for x \in [[0, pi]
+        uniform_real_distribution<double> zero_to_pi(0.0,pi);
+        uniform_real_distribution<double> zero_to_two_pi(0.0,2*pi);
+
+        // Now use the RNG
+        double t1 = double (zero_to_pi(gen));
+        double t2 = double (zero_to_pi(gen));
+        double p1 = double (zero_to_two_pi(gen));
+        double p2 = double (zero_to_two_pi(gen));
+
+
+        /*
+        cout << endl << "printing theta and phi"  << endl;
+        cout << t1 << " " << t2 << endl
+             << p1 << " " << p2 << endl
+             << endl;
+        */
+
+
+        // MC integrating
+        fy = func_important_samp(r1, r2, t1, t2, p1, p2);
+        MCintIS += fy;
+        sum_sigmaIS += fy*fy;
+    }
+
+    // stops the clock
+    high_resolution_clock::time_point t2 = high_resolution_clock::now();
+    duration<double> time_span = duration_cast<duration<double> >(t2 - t1);
+    cout << endl << "Monte Carlo importance sampling used " << time_span.count() << " seconds" << endl;
+    cout << endl;
+
+
+    // setting up the jaocbi
+    double jacobi = 4* pow(pi,4) / 16;
+
+    // calculating the mean integration results and the variance
+    MCintIS= MCintIS / (double (n));
+    sum_sigmaIS = sum_sigmaIS / (double (n));
+    double variance = sum_sigmaIS - MCintIS * MCintIS;
+
+
+
+    // printing results for Important Samplng
+    cout << endl << "Results for MC important sampling" << endl
+         << "Variance = " << variance << endl
+         << "Integral = " << jacobi * MCintIS << endl
+         << "Exact = " << exact_solution << endl;
+
+
+
+}
